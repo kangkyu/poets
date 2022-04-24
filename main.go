35cf9601b3484a2ff2a19f027f2ddd874105583a
kangkyu <kangkyu1111@gmail.com>
2022-04-24 00:51:08 -0700

    http endpoint to send message

--- main.go
+++ main.go
@@ -1,6 +1,11 @@
 package main
 
 import (
+	"net/http"
+	"encoding/json"
+	"fmt"
+	"log"
+
 	"github.com/Shopify/sarama"
 	"github.com/joeshaw/envdecode"
 )
@@ -17,23 +22,61 @@ type AppConfig struct {
 	}
 }
 
+type SubmitBody struct {
+	Message string `json:"message"`
+}
+
+type Application struct {
+	Producer sarama.AsyncProducer
+	Topic string
+}
+
 func main() {
 	appconfig := AppConfig{}
 	envdecode.MustDecode(&appconfig)
 	brokers := []string{appconfig.Kafka.URL}
 	topic := appconfig.Kafka.Topic
 
+	producer := NewProducer(brokers)
+	defer producer.Close()
+
+	app := Application{
+		Producer: producer,
+		Topic: topic,
+	}
+
+	http.HandleFunc("/submit", app.SubmitHandler)
+	log.Println("Listen on localhost:8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func (app Application) SubmitHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+        http.Error(w, "POST only", http.StatusMethodNotAllowed)
+		return
+	}
+
+	var submit SubmitBody
+	err := json.NewDecoder(r.Body).Decode(&submit)
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusBadRequest)
+        return
+    }
+	fmt.Printf("%s\n", submit.Message)
+
+	msg := submit.Message
+	app.Producer.Input() <- &sarama.ProducerMessage{
+		Topic: app.Topic,
+		Value: sarama.StringEncoder(msg),
+	}
+}
+
+func NewProducer(brokers []string) sarama.AsyncProducer {
 	config := sarama.NewConfig()
 	producer, err := sarama.NewAsyncProducer(brokers, config)
 
 	if err != nil {
 		panic(err)
 	}
-	defer producer.Close()
-
-	msg := "Hello world"
-	producer.Input() <- &sarama.ProducerMessage{
-		Topic: topic,
-		Value: sarama.StringEncoder(msg),
-	}
+	return producer
 }
